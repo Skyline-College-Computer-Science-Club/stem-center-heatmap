fe3561b2091f10853ef4a677e28d9a8e92d77bbc
Vincent Ruan <71678250+Vincents-Works-s@users.noreply.github.com>
2023-04-17 17:43:09 -0700

    Add files for new version with Flask.

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    name = request.args.get("name", "world")
+    return render_template("index.html", name=name)
